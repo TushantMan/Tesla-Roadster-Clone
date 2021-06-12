c0b1eb6137f65aa33d89869b0b62f9cbaeedc888
Tushant Man <tushantman77@hotmail.com>
2021-06-13 00:28:00 +0545

    commit

--- js/main.js
+++ js/main.js
@@ -53,14 +53,13 @@ menuBtn.onclick = function(){
   navBar.classList.add("active");
   menuBtn.style.opacity = "0";
   menuBtn.style.pointerEvents = "none";
-  body.style.overflow = "hidden";
   scrollBtn.style.pointerEvents = "none";
 }
 cancelBtn.onclick = function(){
   navBar.classList.remove("active");
   menuBtn.style.opacity = "1";
   menuBtn.style.pointerEvents = "auto";
-  body.style.overflow = "auto";
+  
   scrollBtn.style.pointerEvents = "auto";
 }
 
